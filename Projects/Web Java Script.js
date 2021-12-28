71f7984163bd8638de9bdb0365f19f50326e4353
antoniuswisnu <88194426+antoniuswisnu@users.noreply.github.com>
2021-12-29 00:48:37 +0700

    Delete Web Java Script.js

--- Projects/Web Java Script.js
+++
@@ -1,9 +0,0 @@
-const http = require('http'), host = 'localhost', port = 3000
-
-const node_server = http.createserver((req,res) => {
-    res.end('Hello Fohkin World $(host):$(port)')
-})
-
-node_server.listen(port,host,() =>{
-    console.log('server berhasil di running...')
-})
